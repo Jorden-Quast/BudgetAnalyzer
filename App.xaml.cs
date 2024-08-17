190385d5da97a027761fd7d7e7853c181a6fd9ea
Jorden Quast <jordenquast@gmail.com>
2024-08-17 14:03:04 -0500

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BudgetAnalyzer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetAnalyzer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
