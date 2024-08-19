44fe174c7b5779a0af4e88a551939b828e3fc985
Jorden Quast <jordenquast@gmail.com>
2024-08-19 09:12:56 -0500

    Created Data, Started on edit page

---
+++ BudgetAnalyzer.Shared/Data/BudgetCategory.cs
@@ -0,0 +1,31 @@
+﻿namespace BudgetAnalyzer.Shared.Data;
+
+public record BudgetCategory(string Name, decimal Percentage, decimal? Cutoff) 
+{
+    public static readonly BudgetCategory Default = new("Default Cagetory", 0, null); 
+}
+
+public class Budget : IEquatable<Budget>
+{
+    public string Name { get; init; }
+    public IEnumerable<BudgetCategory> Categories { get; init; }
+
+    public Budget(string name, IEnumerable<BudgetCategory> categories)
+    {
+        Name = name;
+        Categories = categories;
+    }
+
+    public bool Equals(Budget? other)
+    {
+        if(other == null) return false;
+
+        return Name == other.Name
+               && Categories.SequenceEqual(other.Categories);
+    }
+    public override bool Equals(object? obj) => Equals(obj as Budget);
+    public override int GetHashCode() => HashCode.Combine(Name, Categories.GetHashCode());
+
+    public static bool operator ==(Budget? left, Budget? right) => left?.Equals(right) ?? (left is null && right is null);
+    public static bool operator !=(Budget? left, Budget? right) => !(left == right);
+}
--- BudgetAnalyzer.Shared/State/AnalyzerState.cs
+++ BudgetAnalyzer.Shared/State/AnalyzerState.cs
@@ -1,6 +1,10 @@
-﻿namespace BudgetAnalyzer.Shared.State;
+﻿using BudgetAnalyzer.Shared.Data;
+
+namespace BudgetAnalyzer.Shared.State;
 public record AnalyzerState
 {
+    public Budget? CurrentBudget { get; init; }
+
     public int CounterValue { get; init; }
 
     public AnalyzerState() => CounterValue = 0;
