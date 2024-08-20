e05c24c34563bd973c6376aa985069ff822b9d31
Jorden Quast <jordenquast@gmail.com>
2024-08-20 09:59:25 -0500

    Save / Load technically works
    Can save and load files, weird behavior because ID's don't serialize
    
    - Need to add load / export buttons to menu
    - Need to make ID's properties rather than fields so they serialize

--- BudgetAnalyzer.Shared/Data/State/AnalyzerState.cs
+++ BudgetAnalyzer.Shared/Data/State/AnalyzerState.cs
@@ -1,5 +1,6 @@
 ﻿using BudgetAnalyzer.Shared.Data;
 using System.Collections.Immutable;
+using System.Text.Json;
 
 namespace BudgetAnalyzer.Shared.State;
 
@@ -14,4 +15,8 @@ public record AnalyzerState
         AvailableBudgets = ImmutableList.Create([Budget.Default]);
         Settings = new AppSettings(AvailableBudgets.First().Id);
     }
+
+    public string ToJson(bool writeIndented = false) => JsonSerializer.Serialize(this, new JsonSerializerOptions() { WriteIndented = writeIndented});
+    public static AnalyzerState FromJson(string jsonString) => FromJsonOrDefault(jsonString) ?? throw new ArgumentNullException($"{nameof(jsonString)} could not be parsed");
+    public static AnalyzerState? FromJsonOrDefault(string jsonString) => JsonSerializer.Deserialize<AnalyzerState>(jsonString);
 }
--- BudgetAnalyzer.Shared/Data/State/StateManager.cs
+++ BudgetAnalyzer.Shared/Data/State/StateManager.cs
@@ -1,9 +1,12 @@
-﻿namespace BudgetAnalyzer.Shared.State;
+﻿using System.Text.Json;
+
+namespace BudgetAnalyzer.Shared.State;
 
 public class StateManager
 {
-    public AnalyzerState State => _State ?? throw new NullReferenceException($"{nameof(State)} can not be null");
-    private AnalyzerState _State = new();
+    private static readonly string DocPath = AppDomain.CurrentDomain.BaseDirectory + "AppData.json";
+    public AnalyzerState State => _State;
+    private AnalyzerState _State = LoadState();
 
     private event EventHandler<AnalyzerState>? StateChanged;
 
@@ -11,6 +14,26 @@ public class StateManager
     {
         _State = action.UpdateState(_State);
         StateChanged?.Invoke(this, _State);
+        SaveState();
+    }
+    
+    private static AnalyzerState LoadState()
+    {
+        if (!File.Exists(DocPath))
+        {
+            AnalyzerState defaultState = new();
+            File.WriteAllText(DocPath, defaultState.ToJson(true));
+            return defaultState;
+        }
+
+        string serializedState = File.ReadAllText(DocPath);
+        return AnalyzerState.FromJson(serializedState);
+    }
+
+    private void SaveState()
+    {
+        string serializedState = _State.ToJson(true);
+        File.WriteAllText(DocPath, serializedState);
     }
 
     public void Subscribe(EventHandler<AnalyzerState> action) => StateChanged += action;
