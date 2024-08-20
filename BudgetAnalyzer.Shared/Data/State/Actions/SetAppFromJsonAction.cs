21b99c56100d46b0ff2da246b55dd2abf74a19df
Jorden Quast <jordenquast@gmail.com>
2024-08-20 10:38:19 -0500

    Created file upload button
    - Need to implement File Saver (https://learn.microsoft.com/en-us/dotnet/communitytoolkit/maui/essentials/file-saver?tabs=windows)?
    - For debugging and reference, would like to Display app state string in settings page

---
+++ BudgetAnalyzer.Shared/Data/State/Actions/SetAppFromJsonAction.cs
@@ -0,0 +1,7 @@
+﻿namespace BudgetAnalyzer.Shared.State;
+
+/// <summary> Tries to parse the json string, returning a default state if failed </summary>
+public record class SetAppFromJsonAction(string JsonString) : IAction
+{
+    public AnalyzerState UpdateState(AnalyzerState state) => AnalyzerState.FromJsonOrDefault(JsonString) ?? new AnalyzerState();
+}
