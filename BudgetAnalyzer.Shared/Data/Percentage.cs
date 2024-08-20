097ba88fe45a19587b735cb3680f3b8496edc974
Jorden Quast <jordenquast@gmail.com>
2024-08-20 14:37:07 -0500

    Created Percentage class
    - First go at it, basic percentage class going, hopefully it's okay

---
+++ BudgetAnalyzer.Shared/Data/Percentage.cs
@@ -0,0 +1,28 @@
+﻿namespace BudgetAnalyzer.Shared.Data;
+
+/// <summary>
+/// Attempts to make working with percentages similar to how it would be done if writing on math on paper
+/// </summary>
+public readonly struct Percentage
+{
+    public decimal FractionalValue { get; init; }
+    public decimal Value => FractionalValue * 100;
+
+    /// <summary> Creates a new percentage instance. Value should be the numerical percentage ie 10% = "10" </summary>
+    public Percentage(decimal percentValue) => FractionalValue = (percentValue / 100);
+    /// <summary> Converts the percentage to a string, leaving off the '%' character. 10% => "10" </summary>
+    public override string ToString() => (FractionalValue * 100).ToString();
+    public decimal PercentOf(decimal value) => FractionalValue * value;
+
+
+    // Percentage-Number operations
+    public static decimal operator +(Percentage left, decimal right) => (1 + left.FractionalValue) * right;
+    public static decimal operator +(decimal left, Percentage right) => left * (1 + right.FractionalValue);
+
+    public static decimal operator -(decimal left, Percentage right) => left * (1 - right.FractionalValue);
+
+    // Percentage-Percentage operations
+    public static Percentage operator +(Percentage left, Percentage right) => new((left.FractionalValue + right.FractionalValue) * 100);
+    public static Percentage operator -(Percentage left, Percentage right) => new((left.FractionalValue - right.FractionalValue) * 100);
+
+}
