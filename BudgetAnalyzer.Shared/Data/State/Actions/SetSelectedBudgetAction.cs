cdc910d2ec58cae229fd5a8fa8759cb7878b92ba
Jorden Quast <46496434+Jorden-Quast@users.noreply.github.com>
2024-08-20 14:08:16 -0500

    Fixed (#14)

--- BudgetAnalyzer.Shared/Data/State/Actions/SetSelectedBudgetAction.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BudgetAnalyzer.Shared.State;
-
-public record SetSelectedBudgetAction(Guid SelectedBudgetId): IAction
-{
-    public AnalyzerState UpdateState(AnalyzerState state) => state with { Settings = state.Settings with { CurrentBudgetId = SelectedBudgetId } };
-}
