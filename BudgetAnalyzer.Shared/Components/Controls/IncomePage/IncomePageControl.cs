2397f85be01d59133dff5e9a9ca089c0b1d5e952
Jorden Quast <46496434+Jorden-Quast@users.noreply.github.com>
2024-12-24 10:16:24 -0600

    Squashed commit of the following:
    commit 21943ffc5287c71c11fe6a34acc6c389a9d7f64d
    Author: Jorden Quast <46496434+Jorden-Quast@users.noreply.github.com>
    Date:   Tue Dec 24 10:12:42 2024 -0600
    
        Added Tooltip, fleshed out types enum
    
    commit 4e8be411b6b0855de7bd19eae9d497b9dcfae5af
    Author: Jorden Quast <46496434+Jorden-Quast@users.noreply.github.com>
    Date:   Tue Dec 24 09:49:01 2024 -0600
    
        Refactored UI, added respect cutoffs
    
    commit 0ecd4e76c0692cf79c99ac52f4a7c881666f90e8
    Author: Jorden Quast <46496434+Jorden-Quast@users.noreply.github.com>
    Date:   Tue Dec 24 09:34:37 2024 -0600
    
        Created Folder and Income Page Base Class
    
    commit c15c3406855c83ed222d502023789cbb8d1e8dfa
    Author: Jorden Quast <46496434+Jorden-Quast@users.noreply.github.com>
    Date:   Mon Dec 23 17:35:32 2024 -0600
    
        Balancing Control Respects IncomeState
    
    commit fe1f5139b7c399498397c0ae70bdad385284dd78
    Author: Jorden Quast <46496434+Jorden-Quast@users.noreply.github.com>
    Date:   Mon Dec 23 17:27:44 2024 -0600
    
        Custom breakdown respects income state
    
    commit 006d19c73965d858cbb45b31a6d1ae14b9756ffe
    Author: Jorden Quast <46496434+Jorden-Quast@users.noreply.github.com>
    Date:   Mon Dec 23 17:11:33 2024 -0600
    
        StandardBreakdown respects Income State
    
    commit 9f5f20edfbb804f757d3faf906d150531ebf0d39
    Author: Jorden Quast <46496434+Jorden-Quast@users.noreply.github.com>
    Date:   Mon Dec 23 16:33:17 2024 -0600
    
        Renamed control, created income page state

---
+++ BudgetAnalyzer.Shared/Components/Controls/IncomePage/IncomePageControl.cs
@@ -0,0 +1,36 @@
+﻿using BudgetAnalyzer.Shared.Data;
+using BudgetAnalyzer.Shared.State;
+
+namespace BudgetAnalyzer.Shared.Controls;
+
+public abstract class IncomePageControl : SubscribedComponent
+{
+    protected abstract void SetControlToDefault();
+
+    protected void AddBudgetCategories()
+    {
+        IncomeState stateCopy = State.IncomeState;
+        Dictionary<Guid, decimal> balanceCopy = stateCopy.CategoryBalances.ToDictionary();
+        Dictionary<Guid, Percentage> percentageCopy = stateCopy.CustomPercentages.ToDictionary();
+
+        var availableCategories = State.SelectedBudget.Categories;
+        for (int i = 0; i < State.SelectedBudget.Categories.Count; i++)
+        {
+            var category = State.SelectedBudget.Categories[i];
+            if (!balanceCopy.Keys.Contains(category.Id))
+                balanceCopy.Add(category.Id, 0);
+            if (!percentageCopy.Keys.Contains(category.Id))
+                percentageCopy.Add(category.Id, 0);
+        }
+
+        IncomeState updatedState = stateCopy with { CategoryBalances = balanceCopy, CustomPercentages = percentageCopy };
+        AddAction(new SetIncomeStateAction(updatedState));
+    }
+
+    protected void SetCategoryBalance(decimal newValue, Guid id)
+    {
+        var balances = State.IncomeState.CategoryBalances.ToDictionary();
+        balances[id] = newValue;
+        AddAction(new SetIncomeStateAction(State.IncomeState with { CategoryBalances = balances }));
+    }
+}
---
+++ BudgetAnalyzer.Shared/Data/IncomeState.cs
@@ -0,0 +1,22 @@
+﻿namespace BudgetAnalyzer.Shared.Data;
+
+public record class IncomeState
+{
+    public static readonly IncomeState Default = new(0, 0, 0, true, new Dictionary<Guid, Percentage>(), new Dictionary<Guid, decimal>());
+    public decimal StandardIncome { get; init; }
+    public decimal CustomIncome { get; init; }
+    public decimal BankAccountBalance { get; init; }
+    public bool RespectCutoffs { get; init; }
+    public IReadOnlyDictionary<Guid, Percentage> CustomPercentages { get; init; }
+    public IReadOnlyDictionary<Guid, decimal> CategoryBalances { get; init; }
+
+    private IncomeState(decimal standardIncome, decimal customIncome, decimal bankAccountBalance, bool respectCutoffs, IReadOnlyDictionary<Guid, Percentage> customPercentages, IReadOnlyDictionary<Guid, decimal> categoryBalances)
+    {
+        StandardIncome = standardIncome; 
+        CustomIncome = customIncome;
+        BankAccountBalance = bankAccountBalance;
+        RespectCutoffs = respectCutoffs;
+        CustomPercentages = customPercentages;
+        CategoryBalances = categoryBalances;
+    }
+}
